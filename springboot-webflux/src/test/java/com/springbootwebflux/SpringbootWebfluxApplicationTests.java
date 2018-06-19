72fc81e5aa35645a863fdc597437d9f0546c1531
xiaoyang <1483863489@qq.com>
2018-06-19 11:01:00 +0800

    添加单元测试

--- springboot-webflux/src/test/java/com/springbootwebflux/SpringbootWebfluxApplicationTests.java
+++ springboot-webflux/src/test/java/com/springbootwebflux/SpringbootWebfluxApplicationTests.java
@@ -2,15 +2,32 @@ package com.springbootwebflux;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.BootstrapWith;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.web.reactive.function.BodyInserters;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@WebFluxTest
 public class SpringbootWebfluxApplicationTests {
+
+	@Autowired
+	private  WebTestClient webClient;
 
 	@Test
 	public void contextLoads() {
+		User user = new User(4L,"admin4","admin4");
+		webClient.post().uri("/api/user/save")
+				.contentType(MediaType.APPLICATION_JSON)
+				.body(BodyInserters.fromObject(user))
+				.exchange()
+				.expectStatus().isCreated()
+				.expectBody(String.class)
+				.isEqualTo("添加成功");
 	}
 
 }
