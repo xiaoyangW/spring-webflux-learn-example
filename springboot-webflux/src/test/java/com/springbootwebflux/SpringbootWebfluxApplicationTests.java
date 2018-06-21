90cc12c9092a422eca332495873040ed16a80892
xiaoyang <1483863489@qq.com>
2018-06-21 15:12:47 +0800

    test

--- springboot-webflux/src/test/java/com/springbootwebflux/SpringbootWebfluxApplicationTests.java
+++ springboot-webflux/src/test/java/com/springbootwebflux/SpringbootWebfluxApplicationTests.java
@@ -1,5 +1,6 @@
 package com.springbootwebflux;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,12 +12,25 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import org.springframework.web.reactive.function.BodyInserters;
 
+import javax.annotation.PostConstruct;
+import java.util.HashMap;
+import java.util.Map;
+
 @RunWith(SpringRunner.class)
 @WebFluxTest
 public class SpringbootWebfluxApplicationTests {
 
 	@Autowired
 	private  WebTestClient webClient;
+
+	private static Map<Long,User> map = new HashMap<Long,User>(10);
+
+	@BeforeClass
+	public static void init(){
+		map.put(1L,new User(1L,"admin","admin"));
+		map.put(2L,new User(2L,"admin2","admin2"));
+		map.put(3L,new User(3L,"admin3","admin3"));
+	}
 
 	@Test
 	public void contextLoads() {
